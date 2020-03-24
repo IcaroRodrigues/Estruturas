82ea8edf120f9ef8e1e62508eaafb2254e01b935
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2020-03-24 13:02:10 -0300

    Criacao do menu

--- pilha.c
+++ pilha.c
@@ -54,7 +54,7 @@ void imprimir_pilha( No *pilha ) {
 
     if( pilha == NULL ) {
 
-        printf("Pilha esta vazia...\n");
+        printf("Pilha esta vazia...\n\n");
     
     }else{
 
@@ -96,25 +96,52 @@ int tamanho_da_pilha( No *pilha ) {
 
 int main() {
 
-    int opcao;
+    int opcao, valor;
+    char resposta = 'n';
+    No *pilha = NULL;
 
     do{
 
-
+        system("clear");
         printf("Registro de Pilha.\n\n");
         printf("(1) - Inserir na pilha.\n");
         printf("(2) - Remover da pilha.\n");
         printf("(3) - Imprimir pilha.\n\n");
         printf("(0) - Sair.\n\n");
-
+        printf("Digite sua opcao: ");
         scanf("%d", &opcao );
 
+        if( opcao == 1 ) {
+            system("clear");
+            printf("Digite um valor: ");
+            scanf("%d", &valor );
+            pilha = inserir_na_pilha( pilha, valor );
+        
+        }
+        
+        if( opcao == 2 ) {
+
+            pilha = remover_da_pilha( pilha );
+        }
+
+        if( opcao == 3 ) {
+
+            system("clear");
+
+            do{ 
+                int tam = tamanho_da_pilha( pilha );
+                printf("Lista de valores na pilha.                Tamanho da pilha: %d\n\n", tam );
+                imprimir_pilha( pilha );
+                printf("Deseja retornar?\n");
+                printf("(s) - (n): ");
+                scanf(" %c", &resposta );
+                
+            }while( resposta != 's' );
+        }
 
     }while( opcao != 0 );
 
-
-
-
+    system("clear");
 
     return 0;
 }
